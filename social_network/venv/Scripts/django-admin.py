1e3105ef7193c26e5ab9269464dc55b73a177632
Vladislav <plachey1@gmail.com>
2019-08-09 12:54:28 +0300

    fix likes

--- social_network/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\NT Алекс\Desktop\05.08_soc_net\social_network\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
